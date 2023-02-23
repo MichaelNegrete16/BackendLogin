b4e7da56aa1bde9707c8c4fba73773acaf22fc08
Maicol <michael.negrete16@gmail.com>
2023-02-23 14:35:37 -0500

    Configuracion basica para la base de datos

---
+++ index.js
@@ -0,0 +1,19 @@
+const express =  require('express')
+require('dotenv').config()
+
+const cors = require('cors')
+
+
+// Crear el servidor de exprees
+const app = express()
+
+// Declarar Coor para poder consumir el backend
+app.use(cors())
+
+// Iniciar la base de datos
+
+
+// Declarar las rutas
+
+
+// Escuchar peticiones
